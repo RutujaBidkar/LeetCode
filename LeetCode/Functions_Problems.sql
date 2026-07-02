d946f3677b8135db46fbdc923347d9456a43c6ce
RutujaBidkar <Rutujadbidkar@gmail.com>
2026-07-02 21:40:20 +0530

    Practice

--- LeetCode/Functions_Problems.sql
+++ LeetCode/Functions_Problems.sql
@@ -56,5 +56,269 @@ GO
 
 SELECT dbo.fn_GetEmployeeSala(3) AS EmpSalary;
 
+--Create a scalar function that returns the employee's city.
+CREATE FUNCTION fn_getempCity
+(
+   @EmpID INT
+)
+RETURNS VARCHAR(100)
+AS
+BEGIN   
+      DECLARE @City VARCHAR(100)
+      
+      SELECT @City = City 
+      FROM Consumer
+      WHERE EmployeeID= @EmpID
+
+      RETURN @City 
+END;
+GO 
+
+SELECT dbo.fn_getempCity(4) AS EmpCity;
+SELECT *FROM Consumer;
+
+--Create a function that returns the Department of an employee.
+
+CREATE FUNCTION fn_GetEmpDepartment
+(
+   @EmpID INT
+)
+RETURNS VARCHAR(100)
+AS 
+BEGIN 
+      DECLARE @Department VARCHAR(100)
+
+      SELECT @Department = Department
+      From Consumer
+      Where EmployeeID = @EmpID
+
+      RETURN @Department 
+END;
+GO 
+
+SELECT dbo.fn_GetEmpDepartment(4) As DeptEmp;
+
+--Create a function that returns the Annual Salary of an employee.
+CREATE FUNCTION fn_getAnnualSalary
+(
+ @EmpID INT
+ )
+ RETURNS DECIMAL(10,2)
+ AS
+ BEGIN 
+      DECLARE @Salary DECIMAL(10,2)
+      
+      SELECT @Salary = Salary * 12
+      FROM Consumer
+      WHERE EmployeeID = @EmpID
+
+      RETURN @Salary 
+
+ END;
+
+ --Create a function that returns an employee's Bonus based on their salary.
+
+ CREATE FUNCTION fn_getEmpBonu
+ (
+   @EmpID Int
+)
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+     DECLARE @Salary DECIMAl (10,2)
+     DECLARE @Bonus INT
+
+     SELECT @Salary= Salary
+     FROM Consumer
+     WHERE EmployeeID = @EmpID
+
+     IF @Salary >= 60000
+     BEGIN
+     SET @Bonus = 10000
+     END
+
+     ElSE IF @Salary >=50000
+     BEGIN
+     SET @Bonus =7000
+     END
+
+     ELSE 
+     BEGIN
+     SET @Bonus = 5000
+     END
+     RETURN @Bonus 
+END;
+SELECT *FROM Consumer;
+SELECT dbo.fn_getEmpBonu(5) AS BonusSalary;
+
+
+--Create a function that calculates the total salary.
+CREATE FUNCTION fn_TotalSalary
+(
+    @Salary DECIMAL(10,2),
+    @Bonus INT
+)
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+    DECLARE @TotalSalary DECIMAL(10,2);
+
+    SET @TotalSalary = @Salary + @Bonus;
+
+    RETURN @TotalSalary;
+END;
+GO
+
+-- Execute the function
+SELECT dbo.fn_TotalSalary(50000, 7000) AS TotalSalary;
+
+--Create a function that returns the total number of employees.
+CREATE FUNCTION fn_totalEmp()
+
+RETURNS INT
+AS
+BEGIN
+      DECLARE @TotalEmp INT
+
+      SELECT @TotalEmp = COUNT(*)
+      FROM Consumer;
+
+      RETURN @TotalEmp 
+
+END;
+
+SELECT dbo.fn_totalEmp() AS Empsalary;
+
+--Create a function that returns the highest salary.
+CREATE FUNCTION fn_maxsalary()
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+     DECLARE @MaxSalary DECIMAL(10,2)
+
+     SELECT @MaxSalary= Max(salary)
+     FROM Consumer
+
+     RETURN @MaxSalary 
+
+END;
+
+SELECT dbo.fn_maxsalary() AS MaxSalary;
+
+--Create a function that returns the lowest salary from the Consumer table.
+CREATE FUNCTION fn_getMinSalary()
+RETURNS DECIMAl(10,2)
+AS
+BEGIN 
+      DECLARE @MinSalary DECIMAL(10,2)
+
+      SELECT @MinSalary = Min(Salary)
+      FROM Consumer
+
+      RETURN @MinSalary 
+END;
+
+SELECT dbo.fn_getMinSalary() AS MinSalary;
+
+--Create function that returns average salary
+CREATE FUNCTION fn_AvgEmpSalary()
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+     DECLARE @AvgSalary DECIMAL(10,2)
+
+     SELECT @AvgSalary = AVG(Salary)
+     FROM Consumer
+
+     RETURN @AvgSalary
+
+END;
+
+SELECT dbo.fn_AvgEmpSalary() As AvgSalary;
+
+--EXIT OR NOT 
+CREATE FUNCTION fn_CheckEmployeeExists
+(
+    @EmpID INT
+)
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @Exists BIT;
+
+    IF EXISTS (SELECT 1 FROM Consumer WHERE EmployeeID = @EmpID)
+        SET @Exists = 1;
+    ELSE
+        SET @Exists = 0;
+
+    RETURN @Exists;
+END;
+GO
+
+SELECT dbo.fn_CheckEmployeeExists(101) AS EmployeeExists;
+
+--Depart Wise Salary
+CREATE FUNCTION fn_SumSalaryByDept
+(
+    @Dept VARCHAR(50)
+)
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+    DECLARE @TotalSalary DECIMAL(10,2);
+
+    SELECT @TotalSalary = SUM(Salary)
+    FROM Consumer
+    WHERE Department = @Dept;
+
+    RETURN @TotalSalary;
+END;
+GO
+
+SELECT dbo.fn_SumSalaryByDept('IT') AS TotalSalary;
+
+--Salary Status Function
+CREATE FUNCTION fn_SalaryStatus
+(
+    @EmpID INT
+)
+RETURNS VARCHAR(100)
+AS
+BEGIN
+    DECLARE @Salary DECIMAL(10,2);
+    DECLARE @SalaryStatus VARCHAR(100);
+
+    SELECT @Salary = Salary
+    FROM Consumer
+    WHERE EmployeeID = @EmpID;
+
+    IF @Salary >= 60000
+    BEGIN
+        SET @SalaryStatus = 'HIGH';
+    END
+    ELSE
+    BEGIN
+        SET @SalaryStatus = 'MEDIUM';
+    END
+
+    RETURN @SalaryStatus;
+END;
+GO
+
+SELECT dbo.fn_SalaryStatus(104) AS Status;
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
 
 
---
+++ LeetCode/MixSp_Fun_Sql.sql
@@ -0,0 +1,115 @@
+Use CompanyDB
+CREATE TABLE Consumer
+(
+    EmployeeID INT PRIMARY KEY,
+    EmployeeName VARCHAR(100),
+    Department VARCHAR(50),
+    Salary DECIMAL(10,2),
+    Age INT,
+    City VARCHAR(50)
+);
+
+INSERT INTO Consumer (EmployeeID, EmployeeName, Department, Salary, Age, City)
+VALUES
+(101, 'Amit',   'HR',        35000.00, 25, 'Pune'),
+(102, 'Priya',  'IT',        60000.00, 30, 'Mumbai'),
+(103, 'Rahul',  'Finance',   45000.00, 28, 'Nagpur'),
+(104, 'Sneha',  'IT',        70000.00, 35, 'Pune'),
+(105, 'Karan',  'Admin',     40000.00, 40, 'Nashik'),
+(106, 'Neha',   'HR',        38000.00, 27, 'Mumbai'),
+(107, 'Vikas',  'Finance',   55000.00, 32, 'Pune'),
+(108, 'Pooja',  'IT',        65000.00, 29, 'Nagpur'),
+(109, 'Rohit',  'Admin',     42000.00, 38, 'Mumbai'),
+(110, 'Anjali', 'HR',        48000.00, 31, 'Pune');
+
+SELECT  * FROM Consumer;
+
+--Create a scalar function that returns the salary of an employee.
+CREATE FUNCTION fn_getEmpSalary
+(
+   @EmpID INT
+)
+RETURNS DECIMAL (10,2)
+AS
+BEGIN
+     DECLARE @salary DECIMAL (10,2)
+
+     SELECT @Salary = Salary
+     FROM Consumer
+     WHERE EmployeeID = @EmpID
+
+     RETURN @salary 
+
+END;
+
+SELECT dbo.fn_getEmpSalary(105) As EmpSalary ;
+
+SELECT * FROM Consumer;
+
+--Create a stored procedure that displays all employees belonging to a given department.
+CREATE PROCEDURE Emp_Department
+    @Dept VARCHAR(100)
+AS
+BEGIN
+    SELECT EmployeeID,
+           EmployeeName,
+           Department,
+           Salary,
+           Age,
+           City
+    FROM Consumer
+    WHERE Department = @Dept;
+END;
+GO
+
+EXEC Emp_Department 'IT';
+
+--Create a stored procedure that increases the salary of an employee.
+CREATE PROCEDURE Increment
+( 
+     @EmpID INT,
+     @Increment DECIMAl(10,2)
+)
+As
+BEGIN
+      UPDATE Consumer SET 
+      Salary = Salary *@Increment /100
+      WHERE @EmpID
+
+      SELECT * 
+      FROM Consumer
+      WHERE EmployeeID = @EmpID
+END;
+GO
+
+--Create a function that returns the annual salary of an employee.
+CREATE FUNCTION fn_getAnnualSalary
+(
+  @EmpID INT
+)
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+     DECLARE @Salary DECIMAL(10,2)
+
+     SELECT @Salary = Salary * 12 
+     FROM Consumer
+     WHERE EmployeeID = @EmpID
+
+     RETURN @Salary
+
+END;
+
+SELECT dbo.fn_getEmpSalary(102);
+
+--Create a stored procedure that returns the salary of an employee using an OUTPUT parameter.
+CREATE PROCEDURE sp_GetEmployeeSalary
+(
+   @EmpID INT
+)
+
+
+SELECT  *FROM Consumer;
+
+
+
